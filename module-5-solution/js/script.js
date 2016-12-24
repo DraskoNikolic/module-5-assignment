647f989f96263bc66b29cf412cdd9f415d8a572c
Drasko Nikolic <Drasko Nikolic>
2016-12-24 21:43:50 +0100

    final solution

--- module-5-solution/js/script.js
+++ module-5-solution/js/script.js
@@ -105,9 +105,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var randomCategory = chooseRandomCategory(categories);
-      var  randomCategoryShortName = randomCategory.short_name;
-      var chosenCategoryShortName = randomCategoryShortName ;
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
 
 
